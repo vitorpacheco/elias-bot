c3f32109f7c04090888a986e6e992e3b180ad292
Vitor Pacheco <vpacheco.costa@gmail.com>
2022-04-05 14:25:20 -0300

    adicionado o express

--- src/index.js
+++ src/index.js
@@ -1,28 +1,53 @@
-const { Client, Intents } = require('discord.js');
+import { Client, Intents } from 'discord.js'
+import express from 'express'
 
-require('dotenv').config();
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Onde está o Elias?')
+})
+
+app.listen(port, () => {
+  console.log(`Onde está o Elias listening on port ${port}`)
+})
+
+const askElias = (client) => {
+  const channel = client.channels.cache.find(channel => channel.name == "geral")
+  channel.send('tava onde elias?')
+}
 
 const client = new Client({
   intents: [
     Intents.FLAGS.GUILDS,
     Intents.FLAGS.GUILD_PRESENCES
   ]
-});
+})
 
 client.once('ready', () => {
-  client.user.setStatus('invisible');
-  console.log(`Ready! Logged in as ${client.user.tag}`);
-});
+  client.user.setStatus('invisible')
+  console.log(`Ready! Logged in as ${client.user.tag}`)
+})
 
 client.on('presenceUpdate', (oldPresence, newPresence) => {
-  if (newPresence.guild !== undefined && newPresence.guild.id == process.env.GUILD_ID) {
-    if (newPresence.user.username == process.env.BRASH_USERNAME && newPresence.user.discriminator == process.env.BRASH_DISCRIMINATOR) {
-      if (newPresence.status != 'offline') {
-        const channel = client.channels.cache.find(channel => channel.name == "geral");
-        channel.send('tava onde elias?');
-      }
-    }
+  if (newPresence.guild === undefined || newPresence.guild === null) {
+    return
   }
-});
 
-client.login(process.env.DISCORD_TOKEN);
+  if (newPresence.guild.id != process.env.GUILD_ID) {
+    return
+  }
+
+  if (newPresence.status == 'offline') {
+    return
+  }
+
+  if (newPresence.user.username == process.env.BRASH_USERNAME && 
+      newPresence.user.discriminator == process.env.BRASH_DISCRIMINATOR) {
+    askElias(client)
+  }
+})
+
+client.login(process.env.DISCORD_TOKEN)
